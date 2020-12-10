956cb2d3109c453a3722cbc47513b2bed64f7e6b
juliagarant <jgarant10@gmail.com>
2020-12-10 15:21:53 -0500

    Moved all classwork to one folder

--- lab2_104987469.py
+++
@@ -1,8 +0,0 @@
-"""
-Lab 2 for Programming for Beginners
-My First Flowchart
-Julia Garant
-Jan 23 2020
-"""
-favNumber = input("What's your favourite number?")
-print(favNumber + " is a great number!")
